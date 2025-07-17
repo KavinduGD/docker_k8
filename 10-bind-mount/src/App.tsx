5c90f1cb88c974e0ce454b1d48506f8f516190ef
KavinduGD <kavidudharmasiri90@gmail.com>
2025-07-17 20:08:42 +0530

    volumes bind-mounts resourse management

--- 10-bind-mount/src/App.tsx
+++ 10-bind-mount/src/App.tsx
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I am Batman </p>
-      
+        <p>I am Batman!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
