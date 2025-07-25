4eeb10a058bfe4e790365e5b12a02ee0bfc81a39
KavinduGD <kavidudharmasiri90@gmail.com>
2025-07-25 17:41:14 +0530

    docker-compose start

---
+++ 17-docker-compose/backend/src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const healthRouter = require("./routes/health");
+const storeRouter = require("./routes/store");
+
+const app = express();
+
+const port = process.env.LOCALHOST_PORT;
+
+app.use(bodyParser.json());
+
+app.use("/health", healthRouter);
+app.use("/store", storeRouter);
+
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGODB_HOST}/${process.env.KEY_VALUE_DB}`,
+    {
+      auth: {
+        username: process.env.KEY_VALUE_USER,
+        password: process.env.KEY_VALE_PASSWORD,
+      },
+      connectTimeoutMS: 500,
+    }
+  )
+  .then(() => {
+    console.log("Connect to DB");
+    app.listen(port, () => {
+      console.log(`App running on port ${process.env.LOCALHOST_PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
