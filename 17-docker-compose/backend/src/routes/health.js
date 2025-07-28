9a39613d915a0780512b2f3437c266a534fb3599
KavinduGD <kavidudharmasiri90@gmail.com>
2025-07-28 12:02:23 +0530

    docker compose complete

--- 17-docker-compose/backend/src/routes/health.js
+++ 17-docker-compose/backend/src/routes/health.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("up man");
+  res.status(200).send("up man lol 123");
 });
 
 module.exports = router;
